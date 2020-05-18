209961e3d86e552884b863af4a2881dbfce29396
Igor Theodoro <igortheodoro12@gmail.com>
2020-05-18 16:00:07 -0300

    Refatoração do código

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
-const input = document.querySelector("input")
-const show = document.querySelector(".show")
+const input = document.querySelector("input");
 var tempoDeConsumo = 2000;
 
 // O input sempre ficar focado para o usuário sempre poder inserir dados
-$(window).mouseover(function () { 
-  input.focus()
+$(window).mouseover(function () {
+  input.focus();
 });
 
 //Funções do botão para alterar a velocidade de consumo
-$(".decrementar").click(function () { 
-  tempoDeConsumo-=100;
+$(".decrementar").click(function () {
+  tempoDeConsumo -= 100;
   $(".textConsumo").text(`Tempo do consumidor: ${tempoDeConsumo}`);
 });
 
-$(".incrementar").click(function () { 
-  tempoDeConsumo+=100;
+$(".incrementar").click(function () {
+  tempoDeConsumo += 100;
   $(".textConsumo").text(`Tempo do consumidor: ${tempoDeConsumo}`);
 });
 
 //Intervalo em que o valor do input será inserido no H1
 //Este é o produtor
-setInterval(function() { 
-  $(show).text(input.value);
+setInterval(function () {
+  $(".show").text(input.value);
 }, 100);
 
 //Função que faz o papel do consumidor e retirar os dados do input no tempo
-function consumidor(){
-  if(input.value.length > 0){
-    var ultimoCaractere = input.value.charAt(input.value.length-1)
-    var valor = input.value.replace(ultimoCaractere, "")
-  
+function consumidor() {
+  if (input.value.length > 0) {
+    var primeiroCaractere = input.value[0];
+    var valor = input.value.replace(primeiroCaractere, "");
+
     $(input).val(valor);
   }
 
-  setTimeout(consumidor, tempoDeConsumo)
+  setTimeout(consumidor, tempoDeConsumo);
 }
 
-consumidor()
+consumidor();
